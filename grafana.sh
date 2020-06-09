2fd7be079948fbb17354abb9755ecec027e2bb92
Robert Silva <robertsilva.info@gmail.com>
2020-06-09 18:30:43 -0300

    Add grafana

---
+++ grafana.sh
@@ -0,0 +1,3 @@
+chown -R 472:472 /mnt/data-docker/grafana/data && \
+chmod -R 775 /mnt/data-docker/grafana && mkdir -p /mnt/data-docker/grafana/certs && \
+chown -R 472:472 /mnt/data-docker/grafana/certs
